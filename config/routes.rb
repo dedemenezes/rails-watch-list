93b4eec1277297fd193e10704e4635ebfbbbedb9
Andre Menezes <djdedemenezes@gmail.com>
2021-05-28 12:33:35 -0300

    route remake another try

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
 
   root to: "lists#index"
 
-
   resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
 
-  
 end

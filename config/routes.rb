a1c058a84b9a6773a12fbc409306b426febd744b
Andre Menezes <djdedemenezes@gmail.com>
2021-05-28 11:43:14 -0300

    changed database.yml was not commited and exclude #index from resources becaus it's done by root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: "lists#index"
 
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 

27f8952197f8997f7629912d1bf4210ffc061199
Andre Menezes <djdedemenezes@gmail.com>
2021-05-29 18:06:32 -0300

    added routes to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
 
   resources :bookmarks, only: [:destroy]
+  resources :reviews, only: [:destroy]
 
 end

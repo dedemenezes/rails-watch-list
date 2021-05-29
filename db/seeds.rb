62b588e5c3b8abc9acb4b48a0a5ec189bc454c1d
Andre Menezes <djdedemenezes@gmail.com>
2021-05-29 14:19:43 -0300

    movie image card changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ movies['results'].each do |movie_hash|
   movie = Movie.create!(
     title: movie_hash['original_title'],
     overview:  movie_hash['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['backdrop_path']}",
     rating: (6..9).to_a.sample
   )
 

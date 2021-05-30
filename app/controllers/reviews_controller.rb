4d3e15f7c2f0cc6b2845b3881e11c14f7082c3ba
Andre Menezes <djdedemenezes@gmail.com>
2021-05-30 05:04:21 -0300

    turned review into partial, reviews with stars hardcoded for sure rsss, maybe not.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   def new
     @list = List.find(params[:list_id])
     @review = Review.new
+    
   end
 
   def create
@@ -10,9 +11,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.list = @list
     if @review.save
-      redirect_to list_path(@list)
-    else
-      render :new
+      redirect_to list_path(@list)      
     end
   end
 

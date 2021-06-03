6ffadf4d42b8f4ec441e3a676dc99650ba5dfeca
Andre Menezes <djdedemenezes@gmail.com>
2021-06-02 23:14:55 -0300

    validation and associations between user and lists and reviews

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,6 +4,7 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :delete_all
   has_many :reviews, dependent: :delete_all
   has_one_attached :photo
+  belongs_to :user
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
+  belongs_to :user
 
   validates :comment, presence: true
   validates :rating, presence: true

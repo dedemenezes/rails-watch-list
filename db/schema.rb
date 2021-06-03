d440410f433f3793ecab81ec87b8d121f14bb8f6
Andre Menezes <djdedemenezes@gmail.com>
2021-06-02 23:38:55 -0300

    migrations and validations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_232754) do
+ActiveRecord::Schema.define(version: 2021_06_03_021935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_06_02_232754) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -67,7 +69,9 @@ ActiveRecord::Schema.define(version: 2021_06_02_232754) do
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["list_id"], name: "index_reviews_on_list_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -87,5 +91,7 @@ ActiveRecord::Schema.define(version: 2021_06_02_232754) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookmarks", "lists"
   add_foreign_key "bookmarks", "movies"
+  add_foreign_key "lists", "users"
   add_foreign_key "reviews", "lists"
+  add_foreign_key "reviews", "users"
 end

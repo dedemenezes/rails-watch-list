820f1f8ac0382d59b0dcbdd2301995600188bb56
Andre Menezes <djdedemenezes@gmail.com>
2021-06-06 15:58:38 -0300

    fixed photo bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable 
+
+  def display_photo
+    if profile_photo.attached?
+      profile_photo.key
+    else
+      "https://res.cloudinary.com/seguraocode/image/upload/v1623005675/wv4pas1xsurz884a6xi9.webp"
+    end
+  end
 end

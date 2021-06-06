5e7b575dd76dcae673186615ca5b67b8dde93b26
Andre Menezes <djdedemenezes@gmail.com>
2021-06-06 10:16:54 -0300

    CRUD policies

--- app/policies/list_policy.rb
+++ app/policies/list_policy.rb
@@ -18,5 +18,17 @@ class ListPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def destroy?
+    record.user == user
+  end
+  
+  def edit?
+    record.user == user
+  end
+  
+  def update?
+  record.user == user
+  end
   
 end

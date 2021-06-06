bf483cbf32f4eec46333fcf0a925af07ddfe1a27
Andre Menezes <djdedemenezes@gmail.com>
2021-06-06 09:33:03 -0300

    skip devise for movies index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
     if params[:query].present?
       @movies = Movie.search_by_title_and_overview(params[:query])
